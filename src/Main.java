e202b0f4c9cdeab259f0711c466cf2b13571a5f1
conditional <105pkn@mail.ru>
2023-05-12 23:34:03 +0700

    commit 2

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,32 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.printf("Hello world!");
-
-
+//Задача 1
+        System.out.println("Задача 1");
+        int clientOS = 0;
+        if (clientOS == 0) {
+            System.out.println("Установите версию приложения для iOS по ссылке");
+        } else if (clientOS == 1) {
+            System.out.println("Установите версию приложения для Android по ссылке");
+        } else {
+            System.out.println("Тип операционной системы не определен");
+        }
+//Задача 2
+        System.out.println("Задача 2");
+//        int clientOS = 1;
+        int clientDeviceYear = 2015;
+        if (clientOS == 0 && clientDeviceYear < 2015) {
+            System.out.println("Установите облегченную версию приложения для iOS по ссылке");
+        } else if (clientOS == 0 && clientDeviceYear >= 2015) {
+            System.out.println("Установите версию приложения для iOS по ссылке");
+        } else if (clientOS == 1 && clientDeviceYear < 2015) {
+            System.out.println("Установите облегченную версию приложения для Android по ссылке");
+        } else if (clientOS == 0 && clientDeviceYear >= 2015) {
+            System.out.println("Установите версию приложения для Android по ссылке");
+        } else {
+            System.out.println("Тип операционной системы не определен");
+        }
+//Задача 3
+        System.out.println("Задача 3");
 
     }
 }
